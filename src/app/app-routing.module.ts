3668ebd71ce6c7689659835a9b160dfb25364037
ismax <ismax.026@gmail.com>
2022-10-22 21:40:45 -0700

    Se comenzo el enrutamiento del sitio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ContentComponent } from './layout/content/content.component';
+import { AccountComponent } from './layout/account/account.component';
+import { AboutUsComponent } from './layout/about-us/about-us.component';
 
 const routes: Routes = [
-  { 
-    path: '',
-    component: LayoutComponent
-  }
+  { path: '', component: LayoutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'content', component: ContentComponent},
+  { path: 'account', component: AccountComponent},
+  { path: 'about-us', component: AboutUsComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ContentComponent } from './layout/content/content.component';
+import { AboutUsComponent } from './layout/about-us/about-us.component';
+import { AccountComponent } from './layout/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContentComponent,
+    AboutUsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
