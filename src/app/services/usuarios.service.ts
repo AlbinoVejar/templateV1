d13b7a086df4cb715635ac1a5affa518fa5f9e01
ismax <ismax.026@gmail.com>
2022-10-26 20:42:04 -0700

    conection pt. 1

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -3,12 +3,14 @@ import {HttpClient} from '@angular/common/http'
 
 @Injectable({providedIn: 'root'})
 export class UsuarioService {
+  usuarios: any[]=[];
   private readonly API_URL = "localhost:6969/usuarios";
   constructor(
     private http: HttpClient
   ) { }
 
   getUsuarios(){
-    this.http.get(`${this.API_URL}`)
+    this.http.get(`${this.API_URL}`).subscribe( (apiData:any) => (this.usuarios = apiData))
+    return this.usuarios;
   }
 }
